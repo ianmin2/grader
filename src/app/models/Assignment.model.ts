1404e2c91960a643f4925c1f0b47a08a922a0012
Ian Innocent <ianmin2@live.com>
2020-07-24 09:26:11 +0100

    Define Assignment Model

--- src/app/models/Assignment.model.ts
+++ src/app/models/Assignment.model.ts
@@ -1,4 +1,28 @@
 export class Assignment
 {
 
+  public assignment_id : number;
+  public assignment_name : string;
+  public assignment_owner: number;
+  public assignment_created : string;
+  public assignment_due : string;
+  public assignment_last_modified : string;
+  public assignment_summary : string;
+  public assignment_notes : string;
+
+
+  constructor(assignment_id:number, assignment_name:string, assignment_owner:number, assignment_created:string, assignment_due:string, assignment_last_modified:string, assignment_summary:string, assignment_notes:string ) {
+
+    this.assignment_id = assignment_id,
+    this.assignment_name = assignment_name;
+    this.assignment_owner = assignment_owner;
+    this.assignment_created = assignment_created;
+    this.assignment_due = assignment_due;
+    this.assignment_last_modified = assignment_last_modified;
+    this.assignment_summary = assignment_summary;
+    this.assignment_notes = assignment_notes;
+
+  }
+
+
 }
