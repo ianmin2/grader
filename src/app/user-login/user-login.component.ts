6ceec043b3f0b27eaeb7ef112faa7c31b215181d
Ian Innocent <ianmin2@live.com>
2020-09-08 23:20:52 +0100

    Added primitive client-side redirection if auth token is defined

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -14,9 +14,13 @@ export class UserLoginComponent implements OnInit {
   loginForm : FormGroup;
   registrationForm: FormGroup;
 
-  constructor(private http: HttpService, private _localStrorage: LocalStorageService) { }
+  constructor(private http: HttpService, private _localStorage: LocalStorageService) {
+
+  }
 
   ngOnInit(): void {
+
+    this.authenticate();
 
     //@ handle logins
     this.loginForm = new FormGroup({
@@ -41,30 +45,40 @@ export class UserLoginComponent implements OnInit {
     return JSON.parse(JSON.stringify(d));
   }
 
+  private authenticate()
+  {
+    if(this._localStorage.get("auth")!=undefined)
+    {
+      window.location.href = window.location.href.replace(/\/auth/ig, '');
+    }
+  }
+
+  //@ Save the login credentials and perform the login hopping
+  private setAuthCrededentials( data: {response,data:{message,command}},   _localStorage: LocalStorageService )
+  {
+    if(data["response"] == 200)
+    {
+      _localStorage.set("auth",data.data.message);
+      this.authenticate();
+    }
+    else
+    {
+      alert(data.data.message);
+    }
+  }
 
   registerUser(){
 
     let newUser  = this.clone(this.registrationForm.value);
     delete newUser.password_2;
-    this.http.addUser(newUser).subscribe(resp=>{
-      if(resp["response"] == 200)
-      {
-        this._localStrorage.set("auth",resp["data"]["message"]);
-      }
-      else
-      {
-        alert(resp["data"]["message"]);
-      }
-    });
+    this.http.addUser(newUser).subscribe((a:{response,data:{message,command}}) =>this.setAuthCrededentials(a,this._localStorage));
   }
 
   loginUser()
   {
     let userData  =  this.loginForm.value;
-    this.http.login(userData).subscribe(function(d){
-      console.log(`\n\n\AUTH RESPONSE\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=`)
-      console.dir(d);
-    });
+   ;
+    this.http.login(userData).subscribe((a:{response,data:{message,command}}) =>this.setAuthCrededentials(a,this._localStorage));
 
   }
 
